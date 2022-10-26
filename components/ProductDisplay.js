026615e63b730b363d54618a436aadcaf93da823
Shahzad Hussain <shahzadcse@gmail.com>
2022-10-26 10:05:25 +0530

    Vue 101 -initial commit

---
+++ components/ProductDisplay.js
@@ -0,0 +1,127 @@
+app.component("product-display", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  template:
+    /* html */
+    `<div class="product-display">
+        <div class="product-container">
+          <div class="product-image">
+            <img :src="image" />
+          </div>
+          <div class="product-info">
+          
+            <h1>{{ title }}</h1>
+
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock!</p>
+
+            <p> Shipping : {{ shipping }} </p>
+           <!-- -->
+           <product-details :details="details"></product-details>
+           <!-- -->
+
+            <div
+              v-for="(variant, index) in variants"
+              :key="variant.id"
+              @mouseover="updateVariant(index)"
+              class="color-circle"
+              :style="{ backgroundColor: variant.color }"
+            ></div>
+
+            <ul class="inline-list">
+              <li v-for="size in sizes">{{ size }}</li>
+            </ul>
+            
+            <div>
+             <button
+              class="button"
+              :class="{ 
+              disabledButton
+              :
+              !inStock}"
+              v-on:click="addToCart" 
+             
+            >
+              Add to Cart
+            </button>
+
+            <button
+              class="button"
+              :class="{ 
+              disabledButton
+              :
+              !inStock}"
+              v-on:click="removeCart" 
+             
+            >
+              Remove from Cart 
+            </button>
+            </div>
+           
+          </div>
+          <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+          <review-form @review-submitted="addReview"></review-form>
+          
+        </div>
+      </div>`,
+
+  data() {
+    return {
+      product: "Socks",
+      brand: "DXB",
+      selectedVariant: 0,
+      details: ["50% Cotton", "30% Wool", "20% Polyster"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+      sizes: ["S", "M", "L", "XL", "XXL", "XXXL"],
+      reviews: [],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.$emit("add-to-cart", this.variants[this.selectedVariant].id);
+    },
+    removeCart() {
+      this.$emit("remove-from-cart", this.variants[this.selectedVariant].id);
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+    addReview(review) {
+      this.reviews.push(review);
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      }
+      return 2.99;
+    },
+  },
+});
